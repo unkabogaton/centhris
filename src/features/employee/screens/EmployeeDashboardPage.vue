faece55f8a43153c73be26784e231c4a22f8468a
unkabogaton <anthonralph2@gmail.com>
2026-03-03 23:23:15 +0800

    feat: create employee dashboard

---
+++ src/features/employee/screens/EmployeeDashboardPage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useEmployeeStore } from "../stores/employee";
+import EmployeeTable from "../components/EmployeeTable.vue";
+
+const store = useEmployeeStore();
+
+onMounted(() => {
+  store.getEmployees("", 1);
+});
+
+const handleSearch = (value: string) => {
+  store.getEmployees(value, 1);
+};
+
+const handlePageChange = (page: number) => {
+  store.getEmployees("", page);
+};
+</script>
+
+<template>
+  <EmployeeTable
+    :employees="store.employees"
+    :loading="store.loading"
+    :pagination="store.pagination"
+    @search="handleSearch"
+    @changePage="handlePageChange"
+  />
+</template>
